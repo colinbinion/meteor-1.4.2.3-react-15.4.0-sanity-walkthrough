82cd4a1d8be1b5219dedbbb3f2a40fb222f35a77
colinbinion <colin.binion@me.com>
2016-12-01 14:04:13 -0700

    updateing collection records

--- imports/client/App.js
+++ imports/client/App.js
@@ -7,16 +7,23 @@ import Items from '../api/Items';
 class App extends Component {
   addItems(event) {
     event.preventDefault();
-    Items.insert({
-      itemOne: {
-        text: this.refs.itemOne.value.trim(),
-        value: 0,
-      },
-      itemTwo: {
-        text: this.refs.itemTwo.value.trim(),
-        value: 0,
-      }
-    });
+    const itemOne = this.refs.itemOne.value.trim();
+    const itemTwo = this.refs.itemTwo.value.trim();
+    if (itemOne !== '' && itemTwo !== ''){
+      Items.insert({
+        itemOne: {
+          text: itemOne,
+          value: 0,
+        },
+        itemTwo: {
+          text: itemTwo,
+          value: 0,
+        }
+      });
+      this.refs.itemOne.value = '';
+      this.refs.itemTwo.value = '';
+    }
+
   }
   render() {
     return (
@@ -25,7 +32,7 @@ class App extends Component {
           <h1>Colin's Voting Thing</h1>
         </header>
         <main>
-          <form onSubmit={this.addItems.bind(this)}>
+          <form className='new-items' onSubmit={this.addItems.bind(this)}>
             <input type='text' ref='itemOne' />
             <input type='text' ref='itemTwo' />
             <button type='submit'>Add VOTING Topics</button>
--- imports/client/Item.js
+++ imports/client/Item.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import Items from '../api/Items';
 
 export default class Item extends Component {
+  voteOne() {
+    Items.update(this.props.item._id, {
+      $inc: {
+        'itemOne.value': 1
+      }
+    })
+  }
+
+  voteOTwo() {
+    Items.update(this.props.item._id, {
+      $inc: {
+        'itemTwo.value': 1
+      }
+    })
+  }
+
   render() {
     return (
       <div className='item'>
-        <div className='vote-one'>
+        <div className='vote-one' onClick={this.voteOne.bind(this)}>
           <span>{this.props.item.itemOne.value}</span>
           <h3>{this.props.item.itemOne.text}</h3>
         </div>
         <span>vs</span>
-        <div className='vote-two'>
+        <div className='vote-two' onClick={this.voteTwo.bind(this)}>
           <span>{this.props.item.itemTwo.value}</span>
           <h3>{this.props.item.itemTwo.text}</h3>
         </div>
